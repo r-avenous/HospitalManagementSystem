72e20f8ef58bff4b34de4a0026a73034102fe025
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-03-01 11:50:24 +0530

    postgres local connect

--- HospitalManagementSystem/settings.py
+++ HospitalManagementSystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HospitalManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
     }
 }
 
