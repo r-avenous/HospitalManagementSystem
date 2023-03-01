3c878eb5f7c641c5bc81682384cfba8c0cc6f978
jena-shreyas <jenashreyas@gmail.com>
2023-03-02 01:19:40 +0530

    Assn4 initial commit

---
+++ db.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "password123",
+)
+
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE mydb")
