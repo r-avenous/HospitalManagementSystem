80728ec6da4fa81b5c56719543e248d76f02a643
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-03-01 11:23:20 +0530

    init

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def login(request):
+    return render(request, 'core/login.html')
